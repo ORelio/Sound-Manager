ORelio/Sound-Manager

commit 87b12c37647700f8b29bc866fe93a234af4343e4

Offer to load system scheme on change (#20)
When installing Sound Manager, load the active scheme instead of the
default one. Then on further launches, if the active system scheme
has changed, offer to load it in Sound Manager before switching to
the Sound Manager scheme.

SoundManager/Program.cs
@@ -28,7 +28,7 @@ namespace SoundManager
                 switch (args[0].ToLowerInvariant())
                 {
                     case RuntimeConfig.CmdArgumentSetup:
-                        Setup(false, true);
+                        Setup(forceResetSounds: false, systemIntegration: true, offerImportCurrentScheme: false);
                         Environment.Exit(0);
                         break;
 
@@ -78,7 +78,7 @@ namespace SoundManager
                 }
             }
 
-            Setup(false, false);
+            Setup(forceResetSounds: false, systemIntegration: false, offerImportCurrentScheme: importFile == null);
             Application.Run(new FormMain(importFile));
         }
 
@@ -87,18 +87,41 @@ namespace SoundManager
         /// </summary>
         /// <param name="forceResetSounds">Also reset all sounds to their default values</param>
         /// <param name="systemIntegration">Also setup maximum system integration</param>
-        public static void Setup(bool forceResetSounds, bool systemIntegration)
+        /// <param name="offerImportCurrentScheme">Offer to import the active scheme if changed externally</param>
+        public static void Setup(bool forceResetSounds, bool systemIntegration, bool offerImportCurrentScheme)
         {
             bool createDataDir = !Directory.Exists(RuntimeConfig.LocalDataFolder);
 
             if (createDataDir)
                 Directory.CreateDirectory(RuntimeConfig.LocalDataFolder);
 
+            SoundScheme activeScheme = SoundScheme.GetActiveScheme();
+            if (offerImportCurrentScheme && SoundScheme.AlreadySetup() && activeScheme != null && !activeScheme.IsSchemeManager)
+            {
+                if (MessageBox.Show(
+                    String.Format("{0}\n\n{1}", Translations.Get("auto_import_offer_text"), activeScheme.ToString()),
+                    Translations.Get("auto_import_offer_title"),
+                    MessageBoxButtons.YesNo,
+                    MessageBoxIcon.Question) == DialogResult.Yes)
+                {
+                    forceResetSounds = true;
+                }
+            }
+
             SoundScheme.Setup();
 
             if (forceResetSounds || createDataDir)
+            {
+                SchemeMeta.ResetAll();
+                if (activeScheme != null && !activeScheme.IsDefault)
+                {
+                    SchemeMeta.Name = activeScheme.ToString();
+                    SchemeMeta.Author = "";
+                    SchemeMeta.About = "";
+                }
                 foreach (SoundEvent soundEvent in SoundEvent.GetAll())
-                    SoundScheme.CopyDefault(soundEvent);
+                    SoundScheme.CopyDefault(soundEvent, activeScheme);
+            }
 
             SoundScheme.Apply(SoundScheme.GetSchemeSoundManager(), true);
 

SoundManager/FormMain.cs
@@ -887,7 +887,7 @@ namespace SoundManager
             )
             {
                 Program.Uninstall();
-                Program.Setup(true, true);
+                Program.Setup(forceResetSounds: true, systemIntegration: true, offerImportCurrentScheme: false);
                 Process.Start(Application.ExecutablePath);
                 Close();
             }

SoundManager/Lang/eng.ini
@@ -165,3 +165,5 @@ download_schemes_no_tls_title=TLS/HTTPS configuration is inadequate
 download_schemes_no_tls_text=The current HTTPS configuration does not allow downloading. View scheme list in your web browser?
 download_schemes_failed_title=Download failed
 download_schemes_failed_text=Failed to download sound schemes. View scheme list in your web browser?
+auto_import_offer_title=Import this sound scheme?
+auto_import_offer_text=The system sound scheme has changed, would you like to import it? Any unsaved project will be lost.

SoundManager/Lang/fra.ini
@@ -165,3 +165,5 @@ download_schemes_no_tls_title=Configuration TLS/HTTPS insuffisante
 download_schemes_no_tls_text=La configuration HTTPS ne permet pas le téléchargement des thèmes. Afficher la liste dans votre navigateur ?
 download_schemes_failed_title=Échec du téléchargement
 download_schemes_failed_text=Le téléchargement des thèmes a échoué. Afficher la liste dans votre navigateur ?
+auto_import_offer_title=Importer ce thème sonore ?
+auto_import_offer_text=Le thème sonore du système a changé, souhaitez-vous l'importer ? Tout projet non sauvegardé sera perdu.

SoundManager/SoundScheme.cs
@@ -53,9 +53,57 @@ namespace SoundManager
             return displayName;
         }
 
+        /// <summary>
+        /// Whether this sound scheme is the Sound Manager scheme
+        /// </summary>
+        /// <returns>TRUE if the sound scheme is the Sound Manager scheme</returns>
+        public bool IsSchemeManager
+        {
+            get
+            {
+                return this.internalName == SchemeManager;
+            }
+        }
+
+        /// <summary>
+        /// Whether the sound scheme is the system default scheme
+        /// </summary>
+        /// <returns>TRUE if the sound scheme is the default sound scheme</returns>
+        public bool IsDefault
+        {
+            get
+            {
+                return this.internalName == SchemeDefault;
+            }
+        }
+
         // ============================= //
         // == Sound scheme management == //
         // ============================= //
+
+        /// <summary>
+        /// Get active sound scheme for the current user
+        /// </summary>
+        /// <returns>Active Scheme or NULL if data not found</returns>
+        public static SoundScheme GetActiveScheme()
+        {
+            string internalName = RegCurrentUser.OpenSubKey(RegSchemes).GetValue(null) as string;
+            if (internalName != null)
+            {
+                foreach (SoundScheme scheme in GetSchemeList())
+                    if (scheme.internalName == internalName)
+                        return scheme;
+            }
+            return null;
+        }
+
+        /// <summary>
+        /// Check if the "SoundManager" sound scheme already exists in registry
+        /// </summary>
+        public static bool AlreadySetup()
+        {
+            return RegCurrentUser.OpenSubKey(RegNames + SchemeManager, false) != null;
+        }
 
         /// <summary>
         /// Create the "SoundManager" sound scheme in registry
@@ -147,7 +195,7 @@ namespace SoundManager
         /// <summary>
         /// Get the list of all sound schemes in registry
         /// </summary>
-        /// <returns>A list of sound shemes, key is display name, value is internal name</returns>
+        /// <returns>An array of sound shemes</returns>
         public static SoundScheme[] GetSchemeList()
         {
             List<SoundScheme> schemes = new List<SoundScheme>();
@@ -289,8 +337,8 @@ namespace SoundManager
         /// </summary>
         public static void Uninstall()
         {
-            string currentScheme = RegCurrentUser.OpenSubKey(RegSchemes).GetValue(null) as string;
-            if (currentScheme == SchemeManager)
+            SoundScheme currentScheme = GetActiveScheme();
+            if (currentScheme != null && currentScheme.IsSchemeManager)
                 Apply(GetSchemeDefault(), true);
 
             RegistryKey apps = RegCurrentUser.OpenSubKey(RegApps);

SoundManager/Translations.cs
@@ -240,6 +240,8 @@ namespace SharpTools
                 translations["download_schemes_no_tls_text"] = "La configuration HTTPS ne permet pas le téléchargement des thèmes. Afficher la liste dans votre navigateur ?";
                 translations["download_schemes_failed_title"] = "Échec du téléchargement";
                 translations["download_schemes_failed_text"] = "Le téléchargement des thèmes a échoué. Afficher la liste dans votre navigateur ?";
+                translations["auto_import_offer_title"] = "Importer ce thème sonore ?";
+                translations["auto_import_offer_text"] = "Le thème sonore du système a changé, souhaitez-vous l'importer ? Tout projet non sauvegardé sera perdu.";
                 //Ajouter de nouvelles traductions ici
             }
             //Add new languages here as 'else if' blocks
@@ -413,6 +415,8 @@ namespace SharpTools
                 translations["download_schemes_no_tls_text"] = "The current HTTPS configuration does not allow downloading. View scheme list in your web browser?";
                 translations["download_schemes_failed_title"] = "Download failed";
                 translations["download_schemes_failed_text"] = "Failed to download sound schemes. View scheme list in your web browser?";
+                translations["auto_import_offer_title"] = "Import this sound scheme?";
+                translations["auto_import_offer_text"] = "The system sound scheme has changed, would you like to import it? Any unsaved project will be lost.";
                 //Add new translations here
             }
         }

commit 3211c9d49e4ddd1ccc5ff96e22f981945540bc59

Fix setup routine for v1.4.1
Bug introduced in 87b12c37647700f8b29bc866fe93a234af4343e4
Fix included in v1.4.1 release

SoundManager/Program.cs
@@ -93,7 +93,10 @@ namespace SoundManager
             bool createDataDir = !Directory.Exists(RuntimeConfig.LocalDataFolder);
 
             if (createDataDir)
+            {
                 Directory.CreateDirectory(RuntimeConfig.LocalDataFolder);
+                Directory.CreateDirectory(SoundEvent.DataDirectory);
+            }
 
             SoundScheme activeScheme = SoundScheme.GetActiveScheme();
             if (offerImportCurrentScheme && SoundScheme.AlreadySetup() && activeScheme != null && !activeScheme.IsSchemeManager)

SoundManager/SchemeMeta.cs
@@ -169,7 +169,8 @@ namespace SoundManager
                     image.Save(SchemeImageFilePath + ".bmp");
                 }
 
-                File.Delete(SchemeImageFilePath);
+                if (File.Exists(SchemeImageFilePath))
+                    File.Delete(SchemeImageFilePath);
 
                 if (File.Exists(SchemeImageFilePath + ".bmp"))
                     File.Move(SchemeImageFilePath + ".bmp", SchemeImageFilePath);