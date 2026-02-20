ORelio/Sound-Manager

commit b2c7bb874c3e696e399dcbf8056e067758ffe659

Add togglable list view for sound items (#29)
List view shows half less items at once but easier to navigate with
keyboard for accessibility.

Available in context menu for any sound: Switch to List view
Save List view as a setting to remember for next launch.

On first launch, look for the SPI_GETSCREENREADER flag in global System
Parameters. If present, defaut to List view. Otherwise, default to Tiles
view (original default view).

SoundManager/Settings.cs
@@ -40,6 +40,11 @@ namespace SoundManager
         public static HashSet<string> DisabledSoundEvents { get { return _disabledSoundEvents; } }
 
         /// <summary>
+        /// Specify whether the User Interface should display the sound scheme items as list, which is useful for accessibility purposes.
+        /// </summary>
+        public static bool SchemeItemsListView { get; set; }
+
+        /// <summary>
         /// Static class initializer to automatically load settings
         /// </summary>
         static Settings()
@@ -86,6 +91,10 @@ namespace SoundManager
                                             if (SoundEvent.GetAll().Any(e => e.InternalName == soundName))
                                                 DisabledSoundEvents.Add(soundName);
                                         break;
+
+                                    case "schemeitemslistview":
+                                        SchemeItemsListView = INIFile.Str2Bool(setting.Value);
+                                        break;
                                 }
                             }
                             break;
@@ -96,6 +105,8 @@ namespace SoundManager
             {
                 PatchStartupSound = ImageresPatcher.IsPatchingRequired;
                 MissingSoundUseDefault = true;
+                SchemeItemsListView = WindowsParameters.IsScreenReaderActive;
+                Save();
             }
         }
 
@@ -112,6 +123,7 @@ namespace SoundManager
             settings["Main"]["ConvertProprietaryFiles"] = ConvertProprietaryFiles.ToString();
             settings["Main"]["PreferStartupSoundOnLogon"] = PreferStartupSoundOnLogon.ToString();
             settings["Main"]["DisabledSoundEvents"] = String.Join(",", DisabledSoundEvents);
+            settings["Main"]["SchemeItemsListView"] = SchemeItemsListView.ToString();
 
             INIFile.WriteFile(RuntimeConfig.SettingsFile, settings, RuntimeConfig.AppInternalName + " Configuration File", false);
         }

SoundManager/FormMain.Designer.cs
@@ -32,10 +32,11 @@
             this.soundContextMenu = new System.Windows.Forms.ContextMenuStrip(this.components);
             this.soundContextMenu_Play = new System.Windows.Forms.ToolStripMenuItem();
             this.soundContextMenu_Change = new System.Windows.Forms.ToolStripMenuItem();
-            this.soundContextMenu_OpenLocation = new System.Windows.Forms.ToolStripMenuItem();
             this.soundContextMenu_Reset = new System.Windows.Forms.ToolStripMenuItem();
             this.soundContextMenu_Remove = new System.Windows.Forms.ToolStripMenuItem();
             this.soundContextMenu_Disable = new System.Windows.Forms.ToolStripMenuItem();
+            this.soundContextMenu_OpenLocation = new System.Windows.Forms.ToolStripMenuItem();
+            this.soundContextMenu_ToggleView = new System.Windows.Forms.ToolStripMenuItem();
             this.imageContextMenu = new System.Windows.Forms.ContextMenuStrip(this.components);
             this.imageContextMenu_Change = new System.Windows.Forms.ToolStripMenuItem();
             this.imageContextMenu_Remove = new System.Windows.Forms.ToolStripMenuItem();
@@ -102,7 +103,8 @@
             this.soundContextMenu_Reset,
             this.soundContextMenu_Remove,
             this.soundContextMenu_Disable,
-            this.soundContextMenu_OpenLocation});
+            this.soundContextMenu_OpenLocation,
+            this.soundContextMenu_ToggleView});
             this.soundContextMenu.Name = "soundContextMenu";
             this.soundContextMenu.Size = new System.Drawing.Size(205, 136);
             // 
@@ -120,13 +122,6 @@
             this.soundContextMenu_Change.Text = "CHANGE_SOUND";
             this.soundContextMenu_Change.Click += new System.EventHandler(this.soundContextMenu_Change_Click);
             // 
-            // soundContextMenu_OpenLocation
-            // 
-            this.soundContextMenu_OpenLocation.Name = "soundContextMenu_OpenLocation";
-            this.soundContextMenu_OpenLocation.Size = new System.Drawing.Size(204, 22);
-            this.soundContextMenu_OpenLocation.Text = "OPEN_FILE_LOCATION";
-            this.soundContextMenu_OpenLocation.Click += new System.EventHandler(this.soundContextMenu_OpenLocation_Click);
-            // 
             // soundContextMenu_Reset
             // 
             this.soundContextMenu_Reset.Name = "soundContextMenu_Reset";
@@ -140,13 +135,27 @@
             this.soundContextMenu_Remove.Size = new System.Drawing.Size(204, 22);
             this.soundContextMenu_Remove.Text = "REMOVE_SOUND";
             this.soundContextMenu_Remove.Click += new System.EventHandler(this.soundContextMenu_Remove_Click);
-            //
+            // 
             // soundContextMenu_Disable
-            //
+            // 
             this.soundContextMenu_Disable.Name = "soundContextMenu_Disable";
             this.soundContextMenu_Disable.Size = new System.Drawing.Size(204, 22);
             this.soundContextMenu_Disable.Text = "DISABLE_SOUND";
             this.soundContextMenu_Disable.Click += new System.EventHandler(this.soundContextMenu_Disable_Click);
+            // 
+            // soundContextMenu_OpenLocation
+            // 
+            this.soundContextMenu_OpenLocation.Name = "soundContextMenu_OpenLocation";
+            this.soundContextMenu_OpenLocation.Size = new System.Drawing.Size(204, 22);
+            this.soundContextMenu_OpenLocation.Text = "OPEN_FILE_LOCATION";
+            this.soundContextMenu_OpenLocation.Click += new System.EventHandler(this.soundContextMenu_OpenLocation_Click);
+            // 
+            // soundContextMenu_ToggleView
+            // 
+            this.soundContextMenu_ToggleView.Name = "soundContextMenu_ToggleView";
+            this.soundContextMenu_ToggleView.Size = new System.Drawing.Size(204, 22);
+            this.soundContextMenu_ToggleView.Text = "TOGGLE_VIEW_MODE";
+            this.soundContextMenu_ToggleView.Click += new System.EventHandler(this.soundContextMenu_ToggleView_Click);
             // 
             // imageContextMenu
             // 
@@ -644,6 +653,7 @@
         private System.Windows.Forms.ToolStripMenuItem soundContextMenu_Remove;
         private System.Windows.Forms.ToolStripMenuItem soundContextMenu_Disable;
         private System.Windows.Forms.ToolStripMenuItem soundContextMenu_OpenLocation;
+        private System.Windows.Forms.ToolStripMenuItem soundContextMenu_ToggleView;
         private System.Windows.Forms.ContextMenuStrip imageContextMenu;
         private System.Windows.Forms.ToolStripMenuItem imageContextMenu_Change;
         private System.Windows.Forms.ToolStripMenuItem imageContextMenu_Remove;

SoundManager/FormMain.cs
@@ -66,6 +66,7 @@ namespace SoundManager
             soundContextMenu_Reset.Text = Translations.Get("sound_reset");
             soundContextMenu_Remove.Text = Translations.Get("sound_remove");
             soundContextMenu_Disable.Text = Translations.Get("system_event_disable");
+            soundContextMenu_ToggleView.Text = Translations.Get("scheme_view_list");
             groupBoxImport.Text = Translations.Get("box_import_system_scheme");
             groupBoxSystemIntegration.Text = Translations.Get("box_system_integration");
             checkBoxPatchImageres.Text = Translations.Get("check_box_imageres_patch");
@@ -130,7 +131,7 @@ namespace SoundManager
 
             // Sound event list
 
-            soundList.View = View.LargeIcon;
+            soundList.View = Settings.SchemeItemsListView ? View.Tile : View.LargeIcon;
             soundList.MultiSelect = false;
             soundList.LargeImageList = new ImageList();
             soundList.LargeImageList.ImageSize = new Size(32, 32);
@@ -309,6 +310,7 @@ namespace SoundManager
         private void ShowSoundContextMenu(SoundEvent soundEvent, Point positionOnScreen)
         {
             soundContextMenu_Disable.Text = Translations.Get(soundEvent.Disabled ? "system_event_enable" : "system_event_disable");
+            soundContextMenu_ToggleView.Text = Translations.Get(Settings.SchemeItemsListView ? "scheme_view_tiles" : "scheme_view_list");
             soundContextMenu.Show(positionOnScreen);
         }
 
@@ -704,6 +706,16 @@ namespace SoundManager
                 SoundScheme.CopyDefault(soundEvent);
                 soundContextMenu_Play_Click(sender, e);
             }
+        }
+
+        /// <summary>
+        /// Toggle sound scheme display mode between Tiles (default) and List (more accessible)
+        /// </summary>
+        private void soundContextMenu_ToggleView_Click(object sender, EventArgs e)
+        {
+            Settings.SchemeItemsListView = !Settings.SchemeItemsListView;
+            soundList.View = Settings.SchemeItemsListView ? View.Tile : View.LargeIcon;
+            Settings.Save();
         }
 
         /// <summary>

SoundManager/Lang/eng.ini
@@ -99,6 +99,8 @@ scheme_load_failed_text=This sound scheme failed to load:
 scheme_load_failed_title=Scheme load failure
 scheme_export_failed_text=This sound scheme failed to export:
 scheme_export_failed_title=Scheme load failure
+scheme_view_list=Switch to List view
+scheme_view_tiles=Switch to Tiles view
 browse_failed_text=Failed to launch Windows Explorer:
 browse_failed_title=Failed to browse to location
 reset_warn_text=The default sound scheme will be loaded and the current sounds will be lost.

SoundManager/Lang/fra.ini
@@ -99,6 +99,8 @@ scheme_load_failed_text=Le chargement du thème a échoué :
 scheme_load_failed_title=Echec de chargement du thème
 scheme_export_failed_text=L'export du thème a échoué :
 scheme_export_failed_title=Echec de l'export du thème
+scheme_view_list=Affichage Liste
+scheme_view_tiles=Affichage Mosaïques
 browse_failed_text=Le démarrage de l'Explorateur Windows a échoué :
 browse_failed_title=Echec de la navigation
 reset_warn_text=Le thème par défaut va être chargé et les sons actuels seront perdus.

SoundManager/SoundManager.csproj
@@ -83,6 +83,7 @@
     <Compile Include="Properties\AssemblyInfo.cs" />
     <Compile Include="NativeResource.cs" />
     <Compile Include="RuntimeConfig.cs" />
+    <Compile Include="WindowsParameters.cs" />
     <Compile Include="Settings.cs" />
     <Compile Include="ShellFileType.cs" />
     <Compile Include="SoundArchiveProprietary.cs" />

SoundManager/Translations.cs
@@ -174,6 +174,8 @@ namespace SharpTools
                 translations["scheme_load_failed_title"] = "Echec de chargement du thème";
                 translations["scheme_export_failed_text"] = "L'export du thème a échoué :";
                 translations["scheme_export_failed_title"] = "Echec de l'export du thème";
+                translations["scheme_view_list"] = "Affichage Liste";
+                translations["scheme_view_tiles"] = "Affichage Mosaïques";
                 translations["browse_failed_text"] = "Le démarrage de l'Explorateur Windows a échoué :";
                 translations["browse_failed_title"] = "Echec de la navigation";
                 translations["reset_warn_text"] = "Le thème par défaut va être chargé et les sons actuels seront perdus.";
@@ -349,6 +351,8 @@ namespace SharpTools
                 translations["scheme_load_failed_title"] = "Scheme load failure";
                 translations["scheme_export_failed_text"] = "This sound scheme failed to export:";
                 translations["scheme_export_failed_title"] = "Scheme load failure";
+                translations["scheme_view_list"] = "Switch to List view";
+                translations["scheme_view_tiles"] = "Switch to Tiles view";
                 translations["browse_failed_text"] = "Failed to launch Windows Explorer:";
                 translations["browse_failed_title"] = "Failed to browse to location";
                 translations["reset_warn_text"] = "The default sound scheme will be loaded and the current sounds will be lost.";

SoundManager/WindowsParameters.cs
@@ -1,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.InteropServices;
+using System.ComponentModel;
+
+namespace SharpTools
+{
+    /// <summary>
+    /// Windows API wrapper for SystemParametersInfo
+    /// </summary>
+    /// <remarks>
+    /// Currently only implements the ScreenReader flag.
+    /// https://learn.microsoft.com/en-us/windows/win32/winauto/screen-reader-parameter
+    /// https://learn.microsoft.com/en-us/windows/win32/api/winuser/nf-winuser-systemparametersinfoa
+    /// https://github.com/PowerShell/PowerShell/issues/11751#issuecomment-600120959
+    /// </remarks>
+    class WindowsParameters
+    {
+        private const int SPI_GETSCREENREADER = 0x0046;
+
+        [DllImport("user32", SetLastError = true, CharSet = CharSet.Unicode)]
+        private static extern int SystemParametersInfo(
+            uint uiAction,
+            uint uiParam,
+            IntPtr pvParam,
+            uint fWinIni);
+
+        /// <summary>
+        /// Determine if the "Screen Reader" flag is set in Windows API
+        /// </summary>
+        /// <returns>TRUE if a screen reader is active</returns>
+        public static bool IsScreenReaderActive
+        {
+            get
+            {
+                var ptr = IntPtr.Zero;
+                try
+                {
+                    ptr = Marshal.AllocHGlobal(sizeof(int));
+                    int hr = SystemParametersInfo(
+                        SPI_GETSCREENREADER,
+                        sizeof(int),
+                        ptr,
+                        0);
+
+                    if (hr == 0)
+                    {
+                        throw new Win32Exception(Marshal.GetLastWin32Error());
+                    }
+
+                    return Marshal.ReadInt32(ptr) != 0;
+                }
+                catch
+                {
+                    return false;
+                }
+                finally
+                {
+                    if (ptr != IntPtr.Zero)
+                    {
+                        Marshal.FreeHGlobal(ptr);
+                    }
+                }
+            }
+        }
+    }
+}

commit af7df0e9ad9f3df88807eb8b24084af2a4b66e48

Fix crash when SoundManager folder does not exist (#29)
Bug introduced in b2c7bb874c3e696e399dcbf8056e067758ffe659

SoundManager/Settings.cs
@@ -106,7 +106,6 @@ namespace SoundManager
                 PatchStartupSound = ImageresPatcher.IsPatchingRequired;
                 MissingSoundUseDefault = true;
                 SchemeItemsListView = WindowsParameters.IsScreenReaderActive;
-                Save();
             }
         }
 